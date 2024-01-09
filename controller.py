0d3e5fda97c19dbdf37f182d146d8e7ba5be8254
Simon Roth <e11907576@student.tuwien.ac.at>
2024-01-09 05:15:39 +0100

    Initial commit

---
+++ controller.py
@@ -0,0 +1,74 @@
+import discord
+import torch
+from langchain.memory import ConversationSummaryBufferMemory
+from langchain_community.llms.huggingface_pipeline import HuggingFacePipeline
+import re
+import os
+
+from langchain_core.prompts import PromptTemplate
+from transformers import AutoTokenizer, pipeline, AutoModelForSeq2SeqLM
+from langchain.chains import ConversationChain
+from dotenv import load_dotenv
+
+# Setup the LLM for text generation
+def prepare_llm():
+    # Model changeable, though different Class may be needed than AutoModelForSeq2SeqLM (either from huggingface or loading a local model)
+    model_name = "lmsys/fastchat-t5-3b-v1.0"
+    # Maximum token size of the chat history memory
+    max_memory_size = 1850
+    max_token_generated = 1024
+    # fetch model_name from huggingface
+    # fetch a tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir="./downloaded_models")
+    # Fetch Model
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_name, torch_dtype=torch.bfloat16,
+                                                  trust_remote_code=True, cache_dir="./downloaded_models",
+                                                  device_map="auto")
+    # Create a pipeline with some params
+    pipe = pipeline("text2text-generation", model=model, torch_dtype=torch.bfloat16,
+                    device_map="auto", tokenizer=tokenizer, max_new_tokens=max_token_generated, do_sample=True, temperature=0.7,
+                    top_k=50, top_p=0.95)
+
+    # store history
+    huggingface_pipe = HuggingFacePipeline(pipeline=pipe)
+
+    memory = ConversationSummaryBufferMemory(llm=huggingface_pipe, max_token_limit=max_memory_size)
+    # base conversational prompt for the chatbot, change to change the "personality" of the bot
+    base_prompt = "You are a friendly chatbot who always responds in the style of a pirate."
+    chain = ConversationChain(llm=huggingface_pipe, memory=memory,
+                                         prompt=PromptTemplate(input_variables=['history', 'input'],
+                                                               template=base_prompt + '\n\nCurrent conversation:\n{history}\nHuman: {input}\nAI:'))
+    return chain
+
+
+# Prepare the Neural network
+conversation_buf = prepare_llm()
+load_dotenv()
+# Load token from .env file
+TOKEN = os.getenv("DISCORD_TOKEN")
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_message(message):
+    # Needs refactoring, bad code
+    if re.match('-chat', message.content):
+        input_prompt = message.content[6:]
+        out = conversation_buf.predict(input=input_prompt)
+        # Remove padding tokens from output
+        res = re.sub(r"<+.*(pad)+.*>+|(pad)>+|<+(pad)", "", out)
+        # For some reason, output contains double space
+        res = re.sub(r'\s+', ' ', res)
+        channel = message.channel.id
+        await message.reply(res)
+
+
+@client.event
+async def on_ready():
+    print('Ready')
+
+
+# run the client
+client.run(TOKEN)
