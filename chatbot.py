9927d0127784d447092b7dd875830d7a5f3d5c8a
Simon Roth <e11907576@student.tuwien.ac.at>
2024-01-10 03:38:27 +0100

    updated base model to be 4-bit quantized mistral-instruct-7b

--- chatbot.py
+++ chatbot.py
@@ -6,14 +6,22 @@ import os
 from langchain.memory import ConversationSummaryBufferMemory
 from langchain_community.llms.huggingface_pipeline import HuggingFacePipeline
 from langchain_core.prompts import PromptTemplate
-from langchain.chains import ConversationChain
-from transformers import AutoTokenizer, pipeline, AutoModelForSeq2SeqLM
+from langchain.chains import LLMChain
+from transformers import AutoTokenizer, pipeline, AutoModelForCausalLM, BitsAndBytesConfig
 from dotenv import load_dotenv
+
 
 # Setup the LLM for text generation
 def prepare_llm(base_prompt):
     # Model changeable, though different Class may be needed than AutoModelForSeq2SeqLM (either from huggingface or loading a local model)
-    model_name = "lmsys/fastchat-t5-3b-v1.0"
+    model_name = "mistralai/Mistral-7B-Instruct-v0.1"
+    bnb_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_use_double_quant=True,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_compute_dtype=torch.bfloat16
+    )
+
     # Maximum token size of the chat history memory
     max_memory_size = 1850
     max_token_generated = 1024
@@ -21,40 +29,56 @@ def prepare_llm(base_prompt):
     # fetch a tokenizer
     tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir="./downloaded_models")
     # Fetch Model
-    model = AutoModelForSeq2SeqLM.from_pretrained(model_name, torch_dtype=torch.bfloat16,
-                                                  trust_remote_code=True, cache_dir="./downloaded_models",
-                                                  device_map="auto")
-    # Create a pipeline with some params
-    pipe = pipeline("text2text-generation", model=model, torch_dtype=torch.bfloat16,
-                    device_map="auto", tokenizer=tokenizer, max_new_tokens=max_token_generated, do_sample=True, temperature=0.7,
-                    top_k=50, top_p=0.95)
+    model = AutoModelForCausalLM.from_pretrained(model_name,
+                                                 trust_remote_code=True, cache_dir="./downloaded_models",
+                                                 device_map="auto", quantization_config=bnb_config)
+    # Tweak these params to change performance
+    pipe = pipeline(
+        model=model,
+        tokenizer=tokenizer,
+        task="text-generation",
+        temperature=0.7,
+        repetition_penalty=1.4,
+        return_full_text=True,
+        max_new_tokens=1024,
+        do_sample=True,
+    )
 
     # store history
     huggingface_pipe = HuggingFacePipeline(pipeline=pipe)
+    prompt_template = (
+            "Current Conversation:{history} \n\n" +
+            "Instruction: " + base_prompt + "\n\n" +
+            "QUESTION:\n" +
+            "{input}" + "\n\n" +
+            "Answer:\n"
+    )
 
     memory = ConversationSummaryBufferMemory(llm=huggingface_pipe, max_token_limit=max_memory_size)
     # base conversational prompt for the chatbot, change to change the "personality" of the bot
-    chain = ConversationChain(llm=huggingface_pipe, memory=memory,
-                                         prompt=PromptTemplate(input_variables=['history', 'input'],
-                                                               template=base_prompt + '\n\nCurrent conversation:\n{history}\nHuman: {input}\nAI:'))
-    return chain
+    chain = LLMChain(verbose=True, llm=huggingface_pipe, memory=memory,
+                     prompt=PromptTemplate(input_variables=['history', 'input'],
+                                           template=prompt_template))
+    return chain, tokenizer
 
 
 # Prepare the Neural network
-base_prompt = "You are a friendly chatbot who always responds like a pirate."
-conversation_buf = prepare_llm(base_prompt)
+# Change this base prompt to change teh "personality" of the chatbot
+base_prompt = "You are a friendly chatbot that speaks like a pirate."
+conversation_buf, tokenizer = prepare_llm(base_prompt)
 load_dotenv()
 # Load token from .env file
 TOKEN = os.getenv("DISCORD_TOKEN")
 intents = discord.Intents.default()
 intents.message_content = True
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 client = discord.Client(intents=intents)
 
 
 @client.event
 async def on_message(message):
     # Needs refactoring, bad code
-    global conversation_buf
+    global conversation_buf, tokenizer, device
     if re.match('-chat', message.content):
         input_prompt = message.content[6:]
         out = conversation_buf.predict(input=input_prompt)
@@ -64,9 +88,10 @@ async def on_message(message):
         res = re.sub(r'\s+', ' ', res)
         await message.reply(res)
     elif re.match('-base_prompt', message.content):
-        content = re.sub('-base_prompt ', "",message.content)
+        content = re.sub('-base_prompt ', "", message.content)
         conversation_buf.prompt = PromptTemplate(input_variables=['history', 'input'],
-                                                               template=content + '\n\nCurrent conversation:\n{history}\nHuman: {input}\nAI:')
+                                                 template=content + '\n\nCurrent conversation:\n{history}\nHuman: {input}\nAI:')
+
 
 @client.event
 async def on_ready():
